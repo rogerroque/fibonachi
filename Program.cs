2390ecc32fb08650aaf04f0fa41f97ad9aa5e040
rogerroque <reroqueo01@elpuig.xeill.net>
2020-12-02 15:10:58 +0100

    el fibonachi

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FibonacciTools;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i >= 0)
+            {
+                Console.Write("Insert an integer (-1 to exit): ");
+                i = int.Parse(Console.ReadLine());
+
+                if (i >= 0)
+                {
+                    Console.Write("Your Fibonnaci is: ");
+                    Console.WriteLine(Fibonacci.GetNumber(i));
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
